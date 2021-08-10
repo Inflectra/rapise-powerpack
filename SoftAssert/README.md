58c21f0cd5989aa3d6bb1c94775cde6397bfc7da
Alexey Grinevich <alexey.grinevich@inflectra.com>
2021-08-10 16:18:56 +0300

    SoftAssertEqual SoftAssertContains

--- SoftAssert/README.md
+++ SoftAssert/README.md
@@ -1,10 +1,17 @@
 ![Download](https://github.githubassets.com/images/icons/emoji/unicode/23ec.png?v8) [Download Now](https://inflectra.github.io/DownGit/#/home?url=https://github.com/Inflectra/rapise-powerpack/tree/master/SoftAssert)
 
-# Tester.SoftAssert and Tester.AssertHasFailures
+# Tester.SoftAssert and Tester.SoftAssertAll
 
 Enables **SoftAssert** feature. SoftAssert proceeds execution without causing test termination (i.e. ignores `StopOnError` option).
 
-Another use-case, is to have a bunch of SoftAssert statements, followed by a real assertion `Tester.AssertHasFailures` that is only fails if there was previous soft failures.
+Another use-case, is to have a bunch of SoftAssert statements, followed by a real assertion `Tester.SoftAssertAll` that is only fails if there was previous soft failures.
+
+Update: SoftAssert was introduced in Rapise v6.7. However, this topic also contains new features:
+```javascript
+Tester.SoftAssertEqual
+Tester.SoftAssertContains
+Tester.SoftAssertNotContains
+```
 
 ## Usage
 
@@ -20,7 +27,7 @@ Copy contents of the [User.js](User.js) into your User.js.
 	Tester.Assert('Pass1', true);
 	
 	// How we should fail if there were failures
-	Tester_AssertHasFailures('Aftermath', true);
+	Tester_SoftAssertAll('Aftermath', true);
 ```
 
 ## Using from RVL
--- SoftAssert/User.js
+++ SoftAssert/User.js
@@ -14,10 +14,99 @@ function Tester_SoftAssert(/**string*/ message, /**boolean*/ condition, /**SeSRe
 	g_stopOnError = stopOnError;
 }
 
-function Tester_AssertHasFailures(/**string*/message)
+function Tester_SoftAssertEqual(/**string*/ message, /**object*/ obj1, /**object*/ obj2, /**SeSReportLink|SeSReportText|SeSReportImage|Object[]*/ data, /**object*/ tags)
+{
+	var stopOnError = g_stopOnError;
+	g_stopOnError = false;
+	
+	Tester.AssertEqual(message, obj1, obj2, data, tags)
+	
+	g_stopOnError = stopOnError;
+}
+
+function Tester_SoftAssertContains(/**string*/ message, /**string*/text, /**string*/substr, /**SeSReportLink|SeSReportText|SeSReportImage|object[]|string*/ data, /**object*/ tags)
+{
+	var stopOnError = g_stopOnError;
+	g_stopOnError = false;
+	
+	var condition = (""+text).indexOf(""+substr)>=0;
+	
+	if(!condition)
+	{
+		if(typeof(data)=="string")
+		{
+			var dataStr = data;
+			data=[new SeSReportText(dataStr)];
+		}
+		
+		if(!data)
+		{
+			data = [];
+		}
+		data.push(new SeSReportText("Substring:"));
+		data.push(new SeSReportText(""+substr));
+		data.push(new SeSReportText("Not found in:"));
+		data.push(new SeSReportText(""+text));
+	}
+	
+	Tester.Assert(message, condition, data, tags)
+	
+	g_stopOnError = stopOnError;
+}
+
+function Tester_SoftAssertNotContains(/**string*/ message, /**string*/text, /**string*/substr, /**SeSReportLink|SeSReportText|SeSReportImage|object[]|string*/ data, /**object*/ tags)
+{
+	var stopOnError = g_stopOnError;
+	g_stopOnError = false;
+	
+	var condition = (""+text).indexOf(""+substr)<0;
+	
+	if(!condition)
+	{
+		if(typeof(data)=="string")
+		{
+			var dataStr = data;
+			data=[new SeSReportText(dataStr)];
+		}
+		
+		if(!data)
+		{
+			data = [];
+		}
+		data.push(new SeSReportText("Substring:"));
+		data.push(new SeSReportText(""+substr));
+		data.push(new SeSReportText("Should not be in:"));
+		data.push(new SeSReportText(""+text));
+	}
+	
+	Tester.Assert(message, condition, data, tags)
+	
+	g_stopOnError = stopOnError;
+}
+
+/**
+ * When an assertion fails, don't stop execution but record the failure. Calling Tester_AssertHasFailures() will execution stop if at least one assertion failed.
+ */
+function Tester_SoftAssert(/**string*/ message, /**boolean*/ condition, /**SeSReportLink|SeSReportText|SeSReportImage|object[]|string*/ data, /**object*/ tags)
+{
+	var stopOnError = g_stopOnError;
+	g_stopOnError = false;
+	
+	Tester.Assert(message, condition, data, tags)
+	
+	g_stopOnError = stopOnError;
+}
+
+
+function Tester_SoftAssertAll(/**string*/message)
 {
 	Tester.Assert(message, Tester.GetTestStatus()==Tester.Pass);
 }
 
 Tester.SoftAssert = Tester_SoftAssert;
-Tester.AssertHasFailures = Tester_AssertHasFailures;
+Tester.SoftAssertContains = Tester_SoftAssertContains;
+Tester.SoftAssertNotContains = Tester_SoftAssertNotContains;
+Tester.SoftAssertEqual = Tester_SoftAssertEqual;
+
+
+Tester.SoftAssertAll = Tester_SoftAssertAll;
