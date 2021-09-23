278ee0eccbc40439d5c13b5b19c5d40cc9ad8ef3
Alexey Grinevich <alexey.grinevich@inflectra.com>
2021-09-23 11:47:45 +0300

    OnTextSaved.cmd demonstration

---
+++ JSBeautify/Main.js
@@ -0,0 +1,29 @@
+//Use 'Record/Learn' button to begin test recording
+function Test(params)
+{
+    f();
+    if (g)
+    {
+        z();
+        if (g)
+        {
+            z();
+            if (g)
+            {
+                if (g)
+                {
+                    z();
+                    if (g)
+                    {
+                        z();
+                        if (g)
+                        {
+                            z();
+                        }
+                    }
+                }
+                z();
+            }
+        }
+    }
+}
