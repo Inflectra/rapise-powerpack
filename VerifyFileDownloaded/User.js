6bf4fc228a054a3c1e4d461420a1aea6d81e1efd
dmarkovtsev <denis@inflectra.com>
2023-01-31 16:06:59 +0300

    SetDownloadFolder added

--- VerifyFileDownloaded/User.js
+++ VerifyFileDownloaded/User.js
@@ -1,9 +1,20 @@
+var __downloadFolder = null;
+
+/**
+ * Set custom download folder.
+ * @param path Download folder path.
+ */
+function SetDownloadFolder( /**string*/ path)
+{
+	__downloadFolder = path;
+}
+
 /**
  * Verify that there are files matching the spec in the downloads folder.
  * Write results to the report.
  * If nothing found before the timeout, returns empty string and add failure to the report.
- * @param {wildcardSpec=*.*} file spec. I.e. *.* or *.pdf
- * @param {timeout=30000} wait time in milliseconds.
+ * @param [wildcardSpec=*.*] file spec. I.e. *.* or *.pdf
+ * @param [timeout=30000] wait time in milliseconds.
  */
 function VerifyFileDownloaded( /**string*/ wildcardSpec, /**number*/ timeout) {
 	var foundFiles = WaitForFileDownloads(wildcardSpec, timeout);
@@ -14,8 +25,8 @@ function VerifyFileDownloaded( /**string*/ wildcardSpec, /**number*/ timeout) {
 /**
  * Wait for any files matching the spec in the downloads folder.
  * If nothing found before the timeout, returns empty string.
- * @param {wildcardSpec=*.*} file spec. I.e. *.* or *.pdf
- * @param {timeout=30000} wait time in milliseconds.
+ * @param [wildcardSpec=*.*] file spec. I.e. *.* or *.pdf
+ * @param [timeout=30000] wait time in milliseconds.
  */
 function WaitForFileDownloads( /**string*/ wildcardSpec, /**number*/ timeout) {
 	timeout = timeout || 30000;
@@ -32,11 +43,12 @@ function WaitForFileDownloads( /**string*/ wildcardSpec, /**number*/ timeout) {
 /**
  * Get all files matching the spec in the downloads folder
  * If nothing found, returns empty string.
- * @param wildcardSpec file spec. I.e. *.* or *.pdf
+ * @param [wildcardSpec=*.*] file spec. I.e. *.* or *.pdf
  */
 function GetFileDownloads( /**string*/ wildcardSpec) {
 	wildcardSpec = wildcardSpec || '*.*';
-	var res = File.Find('%USERPROFILE%\\Downloads\\', wildcardSpec, false,
+	var path = __downloadFolder || '%USERPROFILE%\\Downloads\\';
+	var res = File.Find(path, wildcardSpec, false,
 		false, true, false);
 	Log("Found: " + res);
 	return res;
